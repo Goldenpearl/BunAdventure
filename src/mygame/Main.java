a533d7a0f7a60c4f3d9ce7be882848ade137151e
Goldenpearl <kblair5@yahoo.com>
2016-08-03 00:02:37 -0400

    Added bunny movement

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -10,7 +10,12 @@ import com.jme3.scene.shape.Box;
 import com.jme3.scene.Node;
 import com.jme3.light.DirectionalLight;
 import com.jme3.scene.Spatial;
-
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 /**
  * test
  * @author normenhansen
@@ -21,18 +26,19 @@ public class Main extends SimpleApplication {
         Main app = new Main();
         app.start();
     }
-    protected Geometry player;
+    Spatial bunny;
+    protected Geometry bunnyCube;
     Boolean isRunning=true;
     @Override
     public void simpleInitApp() {
          /** create a blue box at coordinates (1,-1,1) */
-        Box box1 = new Box(1,1,1);
-        player = new Geometry("blue cube", box1);
-        player.setLocalTranslation(new Vector3f(1,-1,1));
+       Box box1 = new Box(1,1,1);
+        Geometry bunnyCube = new Geometry("blue cube", box1);
+        bunnyCube.setLocalTranslation(new Vector3f(1,-1,1));
         Material mat1 = new Material(assetManager, 
                 "Common/MatDefs/Misc/Unshaded.j3md");
         mat1.setColor("Color", ColorRGBA.Blue);
-        player.setMaterial(mat1);
+        bunnyCube.setMaterial(mat1);
     
         /** create a red box straight above the blue one at (1,3,1) */
         Box box2 = new Box(1,1,1);      
@@ -48,7 +54,7 @@ public class Main extends SimpleApplication {
         rootNode.attachChild(pivot); // put this node in the scene
 
         /** Attach the two boxes to the *pivot* node. (And transitively to the root node.) */
-        pivot.attachChild(player);
+        pivot.attachChild(bunnyCube);
         pivot.attachChild(red);
         /** Rotate the pivot node: Note that both boxes have rotated! */
         pivot.rotate(.4f,.4f,0f);
@@ -65,20 +71,72 @@ public class Main extends SimpleApplication {
         rootNode.attachChild(wall);
         
          // Load a model from test_data (OgreXML + material + texture)
-        Spatial ninja = assetManager.loadModel("Models/bun/bun.j3o");
-        ninja.scale(0.35f, 0.35f, 0.35f);
-        ninja.rotate(0.0f, -3.0f, 0.0f);
-        ninja.setLocalTranslation(2.0f, -3.0f, 2.0f);
-        rootNode.attachChild(ninja);
+        bunny = assetManager.loadModel("Models/bun/bun.j3o");
+        bunny.scale(0.35f, 0.35f, 0.35f);
+        bunny.rotate(0.0f, -3.0f, 0.0f);
+        bunny.setLocalTranslation(2.0f, -3.0f, 2.0f);
+        rootNode.attachChild(bunny);
         // You must add a light to make the model visible
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
         rootNode.addLight(sun);
+        initKeys();
     }
+/** Custom Keybinding: Map named actions to inputs. */
+  private void initKeys() {
+    // You can map one or several inputs to one named action
+    inputManager.addMapping("Pause",  new KeyTrigger(KeyInput.KEY_P));
+    inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_H));
+    inputManager.addMapping("Right",  new KeyTrigger(KeyInput.KEY_K));
+    inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_U));
+    inputManager.addMapping("Backwards", new KeyTrigger(KeyInput.KEY_J));
+    inputManager.addMapping("Rotate", new KeyTrigger(KeyInput.KEY_SPACE),
+                                      new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+    // Add the names to the action listener.
+    inputManager.addListener(actionListener,"Pause");
+    inputManager.addListener(analogListener,"Left", "Right", "Forward", "Backwards", "Rotate");
 
+  }
+
+  private ActionListener actionListener = new ActionListener() {
+    public void onAction(String name, boolean keyPressed, float tpf) {
+      if (name.equals("Pause") && !keyPressed) {
+        isRunning = !isRunning;
+      }
+    }
+  };
+
+  private AnalogListener analogListener = new AnalogListener() {
+  
+      public void onAnalog(String name, float value, float tpf) {
+      if (isRunning) {
+        if (name.equals("Rotate")) {
+          bunny.rotate(0, value*speed, 0);
+        }
+        if (name.equals("Right")) {
+          Vector3f v = bunny.getLocalTranslation();
+          bunny.setLocalTranslation(v.x + value*speed, v.y, v.z);
+        }
+        if (name.equals("Left")) {
+          Vector3f v = bunny.getLocalTranslation();
+          bunny.setLocalTranslation(v.x - value*speed, v.y, v.z);
+        }
+        if (name.equals("Forward")) {
+          Vector3f v = bunny.getLocalTranslation();
+          bunny.setLocalTranslation(v.x , v.y, v.z - value*speed);
+        }
+        if (name.equals("Backwards")) {
+          Vector3f v = bunny.getLocalTranslation();
+          bunny.setLocalTranslation(v.x, v.y, v.z + value*speed);
+        }
+      } else {
+        System.out.println("Press P to unpause.");
+      }
+    }
+  };
     @Override
     public void simpleUpdate(float tpf) {
-        player.rotate(0, 2*tpf, 0);
+       // bunnyCube.rotate(0, 2*tpf, 0);
     }
 
     @Override
