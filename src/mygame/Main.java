bc3d4dee5f9ad97da0f2aa32c75b712823578ae4
Goldenpearl <kblair5@yahoo.com>
2016-08-05 08:10:10 -0400

    Updated movement controls.

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -16,8 +16,6 @@ import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
-import com.jme3.scene.CameraNode;
-import com.jme3.scene.control.CameraControl;
 /**
  * test
  * @author normenhansen
@@ -28,14 +26,9 @@ public class Main extends SimpleApplication {
         Main app = new Main();
         app.start();
     }
-    Spatial bunny;
-    protected Geometry bunnyCube;
+    Player player;
     Boolean isRunning=true;
-    int speed =3;
-    Boolean isJumping = false;
-    Float jumpArc = .001f;
-    float bunny_starting_altitude = -3f;
-    float camAbove =1.5f;
+    float camAbove =5.0f;
     float camBehind =3.5f;
     float camCenter = 2.0f;
     @Override
@@ -79,36 +72,37 @@ public class Main extends SimpleApplication {
         wall.setLocalTranslation(2.0f,-2.5f,0.0f);
         rootNode.attachChild(wall);
         
-         // Load a model from test_data (OgreXML + material + texture)
-        bunny = assetManager.loadModel("Models/bun/bun.j3o");
+         // Load a model
+        Spatial bunny = assetManager.loadModel("Models/bun/bun.j3o");
         bunny.scale(0.35f, 0.35f, 0.35f);
         bunny.rotate(0.0f, -3.0f, 0.0f);
-        bunny.setLocalTranslation(2.0f, bunny_starting_altitude, 2.0f);
         rootNode.attachChild(bunny);
         // You must add a light to make the model visible
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
         rootNode.addLight(sun);
         initKeys();
-        Vector3f v = bunny.getLocalTranslation();
-        cam.setLocation(new Vector3f(v.x + camCenter, v.y +camAbove, v.z+camBehind));
+        player = new Player(bunny);
+        rootNode.attachChild(player.getNode());
+        
     }
     
 /** Custom Keybinding: Map named actions to inputs. */
   private void initKeys() {
+       inputManager.deleteMapping( SimpleApplication.INPUT_MAPPING_MEMORY );
     // You can map one or several inputs to one named action
     inputManager.addMapping("Pause",  new KeyTrigger(KeyInput.KEY_P));
-    inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_H));
-    inputManager.addMapping("Right",  new KeyTrigger(KeyInput.KEY_K));
-    inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_U));
-    inputManager.addMapping("Backwards", new KeyTrigger(KeyInput.KEY_J));
+    inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_A));
+    inputManager.addMapping("Right",  new KeyTrigger(KeyInput.KEY_D));
+    inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_W));
+    inputManager.addMapping("Backwards", new KeyTrigger(KeyInput.KEY_S));
     inputManager.addMapping("Rotate", new KeyTrigger(KeyInput.KEY_Y),
                                       new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
     inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
     // Add the names to the action listener.
-    inputManager.addListener(actionListener, "Jump", "Pause");
-    inputManager.addListener(analogListener,"Left", "Right", "Forward", "Backwards", "Rotate");
-
+    inputManager.addListener(actionListener, "Pause");
+    inputManager.addListener(analogListener,"Left", "Right", "Forward", "Backwards", "Rotate",  "Jump");
+      
   }
 
   private ActionListener actionListener = new ActionListener() {
@@ -116,44 +110,26 @@ public class Main extends SimpleApplication {
       if (name.equals("Pause") && !keyPressed) {
         isRunning = !isRunning;
       }
-       if(isRunning && name.equals("Jump"))
-        {
-          triggerJump();
-        }
     }
   };
 
-  private void triggerJump()
-  {
-        if(!isJumping)
-        {
-            isJumping = true;
-        }
-        else isJumping = false;
-        
-  }
   private AnalogListener analogListener = new AnalogListener() {
-  
       public void onAnalog(String name, float value, float tpf) {
       if (isRunning) {
-        if (name.equals("Rotate")) {
-          bunny.rotate(0, value*speed, 0);
-        }
         if (name.equals("Right")) {
-          Vector3f v = bunny.getLocalTranslation();
-          bunny.setLocalTranslation(v.x + value*speed, v.y, v.z);
+           player.rotateRight(value, tpf);
         }
         if (name.equals("Left")) {
-          Vector3f v = bunny.getLocalTranslation();
-          bunny.setLocalTranslation(v.x - value*speed, v.y, v.z);
+          player.rotateLeft(value, tpf);
         }
         if (name.equals("Forward")) {
-          Vector3f v = bunny.getLocalTranslation();
-          bunny.setLocalTranslation(v.x , v.y, v.z - value*speed);
+          player.moveForward(value, tpf);
         }
         if (name.equals("Backwards")) {
-          Vector3f v = bunny.getLocalTranslation();
-          bunny.setLocalTranslation(v.x, v.y, v.z + value*speed);
+          player.moveBackwards(value, tpf);
+        }
+        if (name.equals("Backwards")) {
+          player.jump(value, tpf);
         }
       } else {
         System.out.println("Press P to unpause.");
@@ -162,23 +138,14 @@ public class Main extends SimpleApplication {
   };
     @Override
     public void simpleUpdate(float tpf) {
-       // bunnyCube.rotate(0, 2*tpf, 0);
-        Vector3f v = bunny.getLocalTranslation();
+        player.update();
+        Vector3f v = player.getLocation();
         cam.setLocation(new Vector3f(v.x + camCenter, camAbove, v.z+camBehind));
-        if(isJumping)
-        {
-            bunny.setLocalTranslation(v.x, v.y + jumpArc*speed*2/3, v.z);
-            //TODO shake camera when jumping; dont follow z so closeley
-        }
-        else
-        {
-            if(v.y>bunny_starting_altitude)
-            bunny.setLocalTranslation(v.x, v.y - jumpArc*speed/2, v.z);
-        }
+        //TODO cam rotation
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
-        //TODO: add render code
+        player.render();
     }
 }
--- src/mygame/Player.java
+++ src/mygame/Player.java
@@ -4,10 +4,94 @@
  */
 package mygame;
 
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.Node;
+import com.jme3.math.Quaternion;
 /**
  *
  * @author Kelly
  */
-public class Player {
+public class Player{
+    
+    private static final float BUNNY_STARTING_ALTITUDE=2.0f;
+    private static final Vector3f STARTING_LOCATION = new Vector3f(0.0f, BUNNY_STARTING_ALTITUDE, 0.0f);
+    private static final float BUNNY_RUN_SPEED = 10.0f;
+    private static final float BUNNY_FALL_SPEED = 0.20f;
+    private static final float BUNNY_JUMP_SPEED = 0.20f;
+    private static final float BUNNY_ROTATE_SPEED = 3.0f;
+    private Boolean isJumping;
+    private Spatial playerAvatar;
+    private Node playerNode;
+    public Player(Spatial playerAvatar)
+    { 
+        this.playerAvatar = playerAvatar;
+        isJumping=false;
+        playerNode = new Node();
+        playerNode.attachChild(this.playerAvatar);
+        playerNode.setLocalTranslation(STARTING_LOCATION);
+    }
+    public Spatial getAvatar()
+    {
+        return playerAvatar;
+    }
+    
+    public Node getNode()
+    {
+        return playerNode;
+    }
+    public void render()
+    {
+        
+    }
+    
+    public void setLocation(Vector3f v)
+    {
+        playerNode.setLocalTranslation(v);
+    }
+    
+    public Vector3f getLocation()
+    {
+        return playerNode.getLocalTranslation();
+    }
+    
+    public Quaternion getRotation()
+    {
+        return playerNode.getLocalRotation();
+    }
+    public void moveForward(float keyPressed, float tpf)
+    {
+         Vector3f forward = playerNode.getLocalRotation().getRotationColumn(2);
+         playerNode.move(forward.mult(-1*keyPressed*BUNNY_RUN_SPEED)); //later, mult by tpf, or timeperframe
+    }
+    
+    public void moveBackwards(float keyPressed, float tpf)
+    {
+         Vector3f forward = playerNode.getLocalRotation().getRotationColumn(2);
+         playerNode.move(forward.mult(keyPressed*BUNNY_RUN_SPEED)); //later, mult by tpf, or timeperframe
+    }
+    
+    public void rotateLeft(float keyPressed, float tpf)
+    {
+         playerNode.rotate(0, keyPressed*BUNNY_ROTATE_SPEED, 0);
+    }
+    
+    public void rotateRight(float keyPressed, float tpf)
+    {
+         playerNode.rotate(0, -1.0f*keyPressed *BUNNY_ROTATE_SPEED, 0);
+    }
+    
+    public void jump(float keyPressed, float tpf)
+    {
+         //Vector3f up = playerNode.getLocalRotation().getRotationColumn(1);
+         //playerNode.move(up.mult(keyPressed*BUNNY_RUN_SPEED)); //later, mult by tpf, or timeperframe
+    }
+
+    
+    public void update()
+    {
+       
+    }
+    
     
 }
